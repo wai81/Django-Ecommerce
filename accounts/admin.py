9fc9d660010e6b4a1d73041bd71994450bcf4064
wai81 <vashkelevich.andrei@gmail.com>
2021-07-21 15:54:31 +0300

    14 customise admin-panel 'accounts'

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
+
 # Register your models here.
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    # настройка полей для отображения в админ панели
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    # настройка полей для перехода по ссылке в админ панели
+    list_display_links = ('email', 'first_name', 'last_name')
+    # колонки только для чтения
+    readonly_fields = ('last_login', 'date_joined')
+    # сортирвка по колонке
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
