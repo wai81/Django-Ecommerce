4b8e7515481676d8cf9165947f53b54314eb2ad9
wai81 <vashkelevich.andrei@gmail.com>
2021-07-26 16:08:03 +0300

    28 decrement & Remove Cart Items

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from .models import Cart, CartItem
@@ -42,11 +42,30 @@ def add_cart(request, product_id):
             cart=cart,
         )
         cart_item.save()
+    # переходим в корзину redirect-перенаправляет
     return redirect('cart')
     # return HttpResponse(cart_item.quantity)
     # exit()
-    # переходим в корзину redirect-перенаправляет
 
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
 
 
 def cart(request, total=0, quantity=0, cart_items=None):
@@ -56,7 +75,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        tax = (total * 20)/100  # ндс
+        tax = (total * 20) / 100  # ндс
         grand_total = total + tax
     except ObjectDoesNotExist:
         pass  # just ignore
