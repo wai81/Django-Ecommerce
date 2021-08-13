afe4ba7b352669d35453c7a02457dcc2e0b9601a
wai81 <50743966+wai81@users.noreply.github.com>
2021-08-13 23:31:56 +0300

    39 added variation manager for model variation

--- carts/views.py
+++ carts/views.py
@@ -17,11 +17,10 @@ def _cart_id(request):
 
 # создаем корзину
 def add_cart(request, product_id):
-
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color+' '+size)
-    exit()
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        print(color, size)
 
     product = Product.objects.get(id=product_id)  # get the product
 
