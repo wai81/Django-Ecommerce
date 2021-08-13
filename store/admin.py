d59290ba3767ede3e3845f9282523de04f25b362
wai81 <50743966+wai81@users.noreply.github.com>
2021-08-13 22:45:58 +0300

    38 added model product variation

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Variation
 
 
 # Register your models here.
@@ -9,4 +9,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available')
 
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)  # редактирование в списке
+    list_filter = ('product', 'variation_category', 'variation_value',)
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
+
