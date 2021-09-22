9da35ec54787f5b98af916f46e7d9d945e353871
wai81 <50743966+wai81@users.noreply.github.com>
2021-09-23 00:08:17 +0300

    42 added register.html template viwes, forms.py and function register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from accounts.forms import RegistrationForm
+from accounts.models import Account
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            username = email.split("@")[0]
+
+            user = Account.object.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
