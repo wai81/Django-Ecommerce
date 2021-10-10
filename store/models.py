ce4e2087f24726c7da934a1a32a348f44f2f477d
wai81 <50743966+wai81@users.noreply.github.com>
2021-10-10 15:10:19 +0300

    52 added review count calculate and rating average

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg, Count
 from django.urls import reverse
 
 from accounts.models import Account
@@ -26,6 +27,21 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+    # получаем среднее значение звезд продукта
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('rating'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
 
 class VariationManager(models.Manager):
