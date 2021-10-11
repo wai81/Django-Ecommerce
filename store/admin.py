bbb13998374dcd1c6440644bd72307ab81be8d3b
wai81 <50743966+wai81@users.noreply.github.com>
2021-10-11 20:57:20 +0300

    54 added product gallery and show rating on home page

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,19 @@
+import admin_thumbnails
 from django.contrib import admin
 
-from .models import Product, Variation, ReviewRating
+from .models import Product, Variation, ReviewRating, ProductGallery
 
 
-# Register your models here.
+@admin_thumbnails.thumbnail('image')  # pip install django-admin-thumbnails
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
+
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
     list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available')
+    inlines = [ProductGalleryInline]
 
 
 class VariationAdmin(admin.ModelAdmin):
@@ -18,4 +25,4 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
-
+admin.site.register(ProductGallery)
